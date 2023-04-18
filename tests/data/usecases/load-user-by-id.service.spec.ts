ebc62c4183f8f6323ae21a05254652a14a71886c
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-18 13:22:12 -0300

    feat: ensure LoadUserByIdService be defined

--- tests/data/usecases/load-user-by-id.service.spec.ts
+++ tests/data/usecases/load-user-by-id.service.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DbLoadUserById } from '../../../src/data/usecases/db-load-user-by-id';
+import { LoadUserDataByIdRepositorySpy } from '../mocks';
 
-describe.skip('LoadUserByIdService', () => {
+describe('LoadUserByIdService', () => {
   let service: DbLoadUserById;
+  let loadUserDataByIdRepository: LoadUserDataByIdRepositorySpy;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DbLoadUserById],
+      providers: [
+        DbLoadUserById,
+        {
+          provide: 'LoadUserDataByIdRepository',
+          useClass: LoadUserDataByIdRepositorySpy,
+        }
+      ],
     }).compile();
 
     service = module.get<DbLoadUserById>(DbLoadUserById);
+    loadUserDataByIdRepository = module.get<LoadUserDataByIdRepositorySpy>('LoadUserDataByIdRepository');
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(loadUserDataByIdRepository).toBeDefined();
   });
 });
