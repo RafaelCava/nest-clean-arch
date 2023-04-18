d1bdf3ca0cb7fcadd866ecc33560b1572c1101dd
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-18 13:25:54 -0300

    feat: ensure LoadUserByIdService returns a user on success

--- tests/data/usecases/load-user-by-id.service.spec.ts
+++ tests/data/usecases/load-user-by-id.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DbLoadUserById } from '../../../src/data/usecases/db-load-user-by-id';
 import { LoadUserDataByIdRepositorySpy } from '../mocks';
+import { mockExternalUser, throwError } from '../../domain/mocks';
 
 describe('LoadUserByIdService', () => {
   let service: DbLoadUserById;
@@ -25,4 +26,29 @@ describe('LoadUserByIdService', () => {
     expect(service).toBeDefined();
     expect(loadUserDataByIdRepository).toBeDefined();
   });
+
+  it('should call LoadUserDataByIdRepository with correct values', async () => {
+    const loadByIdSpy = jest.spyOn(loadUserDataByIdRepository, 'loadById');
+    const userId = 'any_id'
+    await service.loadById(userId);
+    expect(loadByIdSpy).toHaveBeenCalledTimes(1);
+    expect(loadUserDataByIdRepository.id).toBe(userId)
+  })
+
+  it('should throw if LoadUserDataByIdRepository throws', async () => {
+    jest.spyOn(loadUserDataByIdRepository, 'loadById').mockImplementationOnce(throwError)
+    const promise = service.loadById('any_id')
+    await expect(promise).rejects.toThrow()
+  })
+
+  it('should return a user on success', async () => {
+    const user = await service.loadById('any_id')
+    expect(user).toEqual(mockExternalUser())
+  })
+
+  it('should return null if LoadUserDataByIdRepository returns null', async () => {
+    jest.spyOn(loadUserDataByIdRepository, 'loadById').mockResolvedValueOnce(Promise.resolve(null))
+    const user = await service.loadById('any_id')
+    expect(user).toBeNull()
+  })
 });
