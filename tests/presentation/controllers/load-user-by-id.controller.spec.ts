d0ad57cc93c5ffdd982ea95e662fd0917ac67ab7
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-18 11:42:37 -0300

    feat: ensure LoadUserByIdController throws if LoadUserById throws

--- tests/presentation/controllers/load-user-by-id.controller.spec.ts
+++ tests/presentation/controllers/load-user-by-id.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LoadUserByIdController } from '../../../src/presentation/controllers/load-user-by-id.controller';
 import { LoadUserByIdSpy } from '../mocks';
+import { throwError, throwException } from '../../domain/mocks';
 
 describe('LoadUserByIdController', () => {
   let sut: LoadUserByIdController;
@@ -32,5 +33,11 @@ describe('LoadUserByIdController', () => {
     await sut.loadById(userId);
     expect(loadUserByIdSpy.userId).toBe(userId);
     expect(loadByIsSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should throws if LoadUserById throws', async () => {
+    jest.spyOn(loadUserByIdSpy, 'loadById').mockImplementationOnce(throwError);
+    const promise = sut.loadById('any_id');
+    await expect(promise).rejects.toThrow(throwException());
   });
 });
